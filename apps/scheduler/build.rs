06f55f45fb287e8463b9050c32d23ff06d42798b
usuario galfrevn <galfre.vn@gmail.com>
2026-03-14 20:47:33 -0300

    feat(scheduler): compile worker, daemon, and gateway protos in build.rs

--- apps/scheduler/build.rs
+++ apps/scheduler/build.rs
@@ -1,11 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .build_client(false)
+        .build_client(true)
         .compile_protos(
             &[
                 "../../proto/agent/v1/common.proto",
                 "../../proto/agent/v1/scheduler.proto",
+                "../../proto/agent/v1/gateway.proto",
+                "../../proto/agent/v1/worker.proto",
+                "../../proto/agent/v1/daemon.proto",
             ],
             &["../../proto"],
         )?;
